166ac0bf307fb737c471bdde9fdc33ebfea41b39
Chaofan Han <fange1206@gmail.com>
2016-11-02 13:12:56 -0700

    Update preprocess.py

--- prepro/preprocess.py
+++ prepro/preprocess.py
@@ -78,66 +78,100 @@ def filterReviews(reviews, minItemNumReviews, minUserNumReviews):
 def filteredReviewsInfo(reviews, minItemNumReviews, minUserNumReviews):    
     t1 = datetime.datetime.now()
     filteredReviews = filterReviews(reviews, minItemNumReviews, minUserNumReviews)
-    print n1, n2, 
+    print 'Mininum num of reviews in each item: ', minItemNumReviews
+    print 'Mininum num of reviews in each user: ', minUserNumReviews
     print 'Dimension of filteredReviews: ', filteredReviews.shape if filteredReviews is not None else '(0, 4)'
-    print 'Num of unique Users: ', finalReviews['userID'].unique().shape
-    print 'Num of unique Product: ', finalReviews['productID'].unique().shape
+    print 'Num of unique Users: ', filteredReviews['userID'].unique().shape[0]
+    print 'Num of unique Product: ', filteredReviews['productID'].unique().shape[0]
     t2 = datetime.datetime.now()
-    print t2 - t1
+    print 'Time elapsed: ', t2 - t1
     return filteredReviews
 
-filteredReviewsBig = filteredReviewsInfo(entry_list, 100, 10)
-filteredReviewsSmall = filteredReviewsInfo(filteredReviewsBig, 150, 15)
+allReviewData = filteredReviewsInfo(entry_list, 100, 10)
+smallReviewData = filteredReviewsInfo(allReviewData, 150, 15)
 
-filteredReviewsSmall['whetherSmall'] = 1
-filteredReviewsSmall = filteredReviewsSmall[['whetherSmall']]
-filteredReviewsBig = pd.merge(filteredReviewsBig, filteredReviewsSmall, left_index=True, right_index=True, how='left')
-filteredReviewsBig['whetherSmall'].fillna(0, inplace=True)
+smallReviewData['whetherSmall'] = 1
+allReviewData = pd.merge(allReviewData, smallReviewData[['whetherSmall']], left_index=True, right_index=True, how='left')
+allReviewData['whetherSmall'].fillna(0, inplace=True)
+allReviewData['whetherSmall'] = allReviewData['whetherSmall'].astype(np.int64)
+allReviewData['score'] = allReviewData['score'].astype(np.int64)
 
-indexYesSmall = np.array(filteredReviewsBig[filteredReviewsBig['whetherSmall'] == 1].index)
-indexNoSmall = np.array(filteredReviewsBig[filteredReviewsBig['whetherSmall'] == 0].index)
-np.random.shuffle(indexYesSmall)
-np.random.shuffle(indexNoSmall)
-indexYesSmallTest = indexYesSmall[:int((len(indexYesSmall)*0.1))]
-indexNoSmallTest = indexNoSmall[:int((len(indexNoSmall)*0.1))]
-indexTest = np.array(list(indexYesSmallTest) + list(indexNoSmallTest))
-filteredReviewsBig['whetherTest'] = 0
-filteredReviewsBig['whetherTest'][filteredReviewsBig.index.isin(indexTest)] = 1
+allReviewData = allReviewData.ix[np.random.permutation(allReviewData.index)]
+allReviewData['whetherTest'] = np.zeros(allReviewData.shape[0], dtype=int)
+splitTrainTest = []
+for name, data in allReviewData.groupby('whetherSmall'):
+    data['whetherTest'][:int(data.shape[0]*0.1)] = 1
+    splitTrainTest.append(data)
+allReviewData = pd.concat(splitTrainTest, axis=0)
 
-userTable = filteredReviewsBig[['userID', 'score']].groupby('userID').mean()
-userTable = userTable.rename(columns={'score': 'avgScore'})
-userTable['uid'] = np.arange(userTable.shape[0])
-userTable.to_csv('user_data.csv', index=False, header=False)
+allReviewData = allReviewData.sort_values(['whetherTest', 'whetherSmall'], ascending=[True, False])
+allReviewData['batchID'] = np.zeros(allReviewData.shape[0], dtype=int)
+allReviewData['batchID'][allReviewData['whetherTest'] == 0] = np.arange(np.sum(allReviewData['whetherTest'] == 0))
+allReviewData['batchID'][allReviewData['whetherTest'] == 1] = np.arange(np.sum(allReviewData['whetherTest'] == 1))
 
-productTable = filteredReviewsBig[['productID', 'score']].groupby('productID').mean()
-productTable = productTable.rename(columns={'score': 'avgScore'})
-productTable['pid'] = np.arange(productTable.shape[0])
-productTable.to_csv('product_data.csv', index=False, header=False)
+productData = pd.DataFrame(allReviewData['productID'].unique(), columns=['productID'])
+productData['pid'] = np.arange(productData.shape[0])
+userData = pd.DataFrame(allReviewData['userID'].unique(), columns=['userID'])
+userData['uid'] = np.arange(userData.shape[0])
 
-filteredReviewsBig = pd.merge(filteredReviewsBig, productTable[['productID', 'pid']], on='productID', how='left')
-filteredReviewsBig = pd.merge(filteredReviewsBig, userTable[['userID', 'uid']], on='userID', how='left')
-del filteredReviewsBig['productID'], filteredReviewsBig['userID']
-reviewTable = filteredReviewsBig
-reviewTable['whetherSmall'] = reviewTable['whetherSmall'].apply(lambda x: int(x))
-reviewTable = reviewTable.sort_values(['whetherTest', 'whetherSmall'], ascending=[True, False])
-reviewTable['batchID'] = 0
-reviewTable['batchID'][reviewTable['whetherTest'] == 0] = np.arange(np.sum(reviewTable['whetherTest'] == 0))
-reviewTable['batchID'][reviewTable['whetherTest'] == 1] = np.arange(np.sum(reviewTable['whetherTest'] == 1))
-reviewTable['rid'] = np.arange(reviewTable.shape[0])
-reviewTable = reviewTable[['rid', 'pid', 'uid', 'score', 'reviewTime', 'whetherTest', 'whetherSmall', 'batchID']]
-reviewTable.to_csv('final_review_data.csv', index=False, header=False)
+allReviewData = pd.merge(allReviewData, productData, on='productID', how='left')
+allReviewData = pd.merge(allReviewData, userData, on='userID', how='left')
+allReviewData['rid'] = np.arange(allReviewData.shape[0])
+del allReviewData['productID'], allReviewData['userID']
+allReviewData = allReviewData[['rid', 'pid', 'uid', 'score', 'reviewTime', 'whetherTest', 'whetherSmall', 'batchID']]
 
+productData.to_csv('product_data.csv', index=False)
+userData.to_csv('user_data.csv', index=False)
+allReviewData.to_csv('final_review_data.csv', index=False, header=False)
 
+uAvgScoreTrain = allReviewData[allReviewData['whetherTest'] == 0][['uid', 'score']].groupby('uid').mean()
+pAvgScoreTrain = allReviewData[allReviewData['whetherTest'] == 0][['pid', 'score']].groupby('pid').mean()
+
+uAvgScoreTrainSmall = allReviewData[(allReviewData['whetherTest'] == 0) & 
+                                    (allReviewData['whetherSmall'] == 1)][['uid', 'score']].groupby('uid').mean()
+pAvgScoreTrainSmall = allReviewData[(allReviewData['whetherTest'] == 0) & 
+                                    (allReviewData['whetherSmall'] == 1)][['pid', 'score']].groupby('pid').mean()
+
+uAvgScoreTrain = uAvgScoreTrain.rename(columns={'score': 'avgScoreTrain'})
+uAvgScoreTrainSmall = uAvgScoreTrainSmall.rename(columns={'score': 'avgScoreTrainSmall'})
+pAvgScoreTrain = pAvgScoreTrain.rename(columns={'score': 'avgScoreTrain'})
+pAvgScoreTrainSmall = pAvgScoreTrainSmall.rename(columns={'score': 'avgScoreTrainSmall'})
+
+uAvgScore = pd.concat([uAvgScoreTrain, uAvgScoreTrainSmall], axis=1)
+pAvgScore = pd.concat([pAvgScoreTrain, pAvgScoreTrainSmall], axis=1)
+uAvgScore.fillna(-1, inplace=True)
+pAvgScore.fillna(-1, inplace=True)
+pAvgScore = pAvgScore.applymap(lambda x: round(x, 6))
+uAvgScore = uAvgScore.applymap(lambda x: round(x, 6))
+
+uAvgScore.to_csv('uAvgScore.csv', header=False)
+pAvgScore.to_csv('pAvgScore.csv', header=False)
 
 connection = MyriaConnection(rest_url='http://demo.myria.cs.washington.edu:8753')
 
+name_uAvgScore = {'userName': 'public',
+                  'programName': 'CSE544_SM_CH',
+                  'relationName': 'uAvgScore'} 
+schema_uAvgScore = {"columnNames": ['uid', 'avgScoreTrain', 'avgScoreTrainSmall'],
+                    "columnTypes": ['LONG_TYPE', 'FLOAT_TYPE', 'FLOAT_TYPE']}
+with open('uAvgScore.csv') as f:
+    connection.upload_fp(name_uAvgScore, schema_uAvgScore, f)
+
+    
+name_pAvgScore = {'userName': 'public',
+                  'programName': 'CSE544_SM_CH',
+                  'relationName': 'pAvgScore'} 
+schema_pAvgScore = {"columnNames": ['pid', 'avgScoreTrain', 'avgScoreTrainSmall'],
+                    "columnTypes": ['LONG_TYPE', 'FLOAT_TYPE', 'FLOAT_TYPE']}
+with open('pAvgScore.csv') as f:
+    connection.upload_fp(name_pAvgScore, schema_pAvgScore, f)
+    
+
 name_review = {'userName': 'public',
                'programName': 'CSE544_SM_CH',
-               'relationName': 'ReviewTable'} 
+               'relationName': 'ReviewData'} 
 schema_review = {"columnNames": ['rid', 'pid', 'uid', 'score', 'reviewTime', 'whetherTest', 'whetherSmall', 'batchID'],
                  "columnTypes": ['LONG_TYPE', 'LONG_TYPE', 'LONG_TYPE', 'FLOAT_TYPE', 'STRING_TYPE', 'INT_TYPE', 'INT_TYPE', 'LONG_TYPE']}
-
-# Now upload that file to Myria
 with open('final_review_data.csv') as f:
     connection.upload_fp(name_review, schema_review, f)
-    
+
