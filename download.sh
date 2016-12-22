55b991dfedda21c0565318efb422f1fa6561ecef
Chaofan Han <fange1206@gmail.com>
2016-12-21 20:29:17 -0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 SOURCE_DIR="$HOME/data"
 NAME="amazon-review"
-wget http://snap.stanford.edu/data/amazon/all.txt.gz -O $SOURCE_DIR/amazon.txt.gz
+wget http://snap.stanford.edu/data/amazon/all.txt.gz -O $SOURCE_DIR/kcore_5.json.gz
+wget http://snap.stanford.edu/data/amazon/productMeta.txt.gz -O $SOURCE_DIR/metadata.json.gz
